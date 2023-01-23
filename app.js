94559de66b1b0bc2945c25653e625e93047ab1f5
dagmawig <dgebreselasse@gmail.com>
2023-01-22 19:08:44 -0600

    added data.js, added loadData router

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const Data = require('./data');
 require('dotenv').config();
 
 const API_PORT = 3001;
@@ -33,3 +33,33 @@ db.once("open", () => console.log("connected to database"));
 
 // checks if connection with the database is successful
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
+router.post("/loadData", (req, res) => {
+    const { userID, email } = req.body;
+
+    console.log(userID)
+
+    Data.findOne({ userID: userID }, (err, data) => {
+        if (err) res.json({ success: false, err: err });
+
+        if (!data) {
+            let data = new Data();
+            data.userID = userID;
+            data.email = email;
+            console.log("new data: ", data);
+            data.save(err => {
+                if (err) res.json({ success: false, err: err });
+                res.json({ success: true, data: data });
+            })
+        } else {
+            res.json({ success: true, data: data });
+        }
+    });
+});
+
+// append /api for our http requests
+app.use("/", router);
+
+// launch our backend into a port
+app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
