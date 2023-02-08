fd00af9e185fc2909c49c779d3cb43346aa9b446
dagmawig <dgebreselasse@gmail.com>
2023-02-07 19:32:05 -0600

    added error message for when userID is invalid, added method to add user email for existing users, added a router to reset user exercise data

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // router to load user data
 router.post("/loadData", (req, res) => {
     const { userID, email } = req.body;
-    if(userID==='' || userID===undefined) res.json({success:false, err: "not the right userID"})
+    if (userID === '' || userID === undefined) res.json({ success: false, err: "not the right userID" })
     Data.findOne({ userID: userID }, (err, data) => {
         if (err) res.json({ success: false, err: err });
 
@@ -53,195 +53,197 @@ router.post("/loadData", (req, res) => {
         } else {
             if (data.email === "") {
                 Data.findOneAndUpdate(
-                  { userID: userID },
-                  {
-                    $set: {
-                      email: email,
+                    { userID: userID },
+                    {
+                        $set: {
+                            email: email,
+                        },
                     },
-                  },
-                  { new: true },
-                  (err, data) => {
-                    if(err) res.json ({success: false, err: err});
-                    else res.json({success: true, data:data})
-                  }
+                    { new: true },
+                    (err, data) => {
+                        if (err) res.json({ success: false, err: err });
+                        else res.json({ success: true, data: data })
+                    }
                 );
-              }
-              else res.json({ success: true, data: data });
+            }
+            else res.json({ success: true, data: data });
         }
     });
 });
 
+
+// router to reset user workout data
 router.post("/resetData", (req, res) => {
-    const {userID} = req.body;
-    
+    const { userID } = req.body;
+
     Data.findOne(
-    {userID: userID}, (err, data) => {
-      if(err) res.json({success: false, err:err})
-      else {     
-        Data.findOneAndUpdate(
-          {userID: userID},
-          {
-            $set: {
-              templateArr: [],
-              fixTempArr: [
-            {
-              tempID: "2023-01-19T23:46:30.547Z",
-              workoutTimeArr: [],
-              name: "Back and Biceps",
-              exerList: [
-                {
-                  bodyPart: "upper legs",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0032.gif",
-                  id: "0032",
-                  name: "barbell deadlift",
-                  target: "glutes",
-                  localUrl: "107.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 3,
-                },
-                {
-                  bodyPart: "back",
-                  equipment: "cable",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0861.gif",
-                  id: "0861",
-                  name: "cable seated row",
-                  target: "upper back",
-                  localUrl: "376.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 3,
-                },
-                {
-                  bodyPart: "back",
-                  equipment: "cable",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0150.gif",
-                  id: "0150",
-                  name: "cable bar lateral pulldown",
-                  target: "lats",
-                  localUrl: "272.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 3,
-                },
-                {
-                  bodyPart: "upper arms",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0031.gif",
-                  id: "0031",
-                  name: "barbell curl",
-                  target: "biceps",
-                  localUrl: "106.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 3,
-                },
-              ],
-            },
-            {
-              tempID: "2023-01-19T23:49:32.364Z",
-              workoutTimeArr: [],
-              name: "Strong 5X5 - Workout A",
-              exerList: [
-                {
-                  bodyPart: "upper legs",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0043.gif",
-                  id: "0043",
-                  name: "barbell full squat",
-                  target: "glutes",
-                  localUrl: "120.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 5,
-                },
-                {
-                  bodyPart: "chest",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0025.gif",
-                  id: "0025",
-                  name: "barbell bench press",
-                  target: "pectorals",
-                  localUrl: "98.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 5,
-                },
-                {
-                  bodyPart: "back",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0027.gif",
-                  id: "0027",
-                  name: "barbell bent over row",
-                  target: "upper back",
-                  localUrl: "101.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 5,
-                },
-              ],
-            },
-            {
-              tempID: "2023-01-19T23:51:37.898Z",
-              workoutTimeArr: [],
-              name: "Strong 5X5 - Workout B",
-              exerList: [
-                {
-                  bodyPart: "upper legs",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0043.gif",
-                  id: "0043",
-                  name: "barbell full squat",
-                  target: "glutes",
-                  localUrl: "120.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 5,
-                },
-                {
-                  bodyPart: "shoulders",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0091.gif",
-                  id: "0091",
-                  name: "barbell seated overhead press",
-                  target: "delts",
-                  localUrl: "190.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 5,
-                },
-                {
-                  bodyPart: "upper legs",
-                  equipment: "barbell",
-                  gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0032.gif",
-                  id: "0032",
-                  name: "barbell deadlift",
-                  target: "glutes",
-                  localUrl: "107.gif",
-                  metric: "wr",
-                  timeStamp: [],
-                  sets: 1,
-                },
-              ],
-            },
-          ],
-              workoutObj: {},
-              record: {}
-            }
-          },
-          {new: true},
-          (err, data) => {
-            if(err) res.json({success: false, err:err})
+        { userID: userID }, (err, data) => {
+            if (err) res.json({ success: false, err: err })
             else {
-              return res.json({success: true, data: data})
+                Data.findOneAndUpdate(
+                    { userID: userID },
+                    {
+                        $set: {
+                            templateArr: [],
+                            fixTempArr: [
+                                {
+                                    tempID: "2023-01-19T23:46:30.547Z",
+                                    workoutTimeArr: [],
+                                    name: "Back and Biceps",
+                                    exerList: [
+                                        {
+                                            bodyPart: "upper legs",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0032.gif",
+                                            id: "0032",
+                                            name: "barbell deadlift",
+                                            target: "glutes",
+                                            localUrl: "107.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 3,
+                                        },
+                                        {
+                                            bodyPart: "back",
+                                            equipment: "cable",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0861.gif",
+                                            id: "0861",
+                                            name: "cable seated row",
+                                            target: "upper back",
+                                            localUrl: "376.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 3,
+                                        },
+                                        {
+                                            bodyPart: "back",
+                                            equipment: "cable",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0150.gif",
+                                            id: "0150",
+                                            name: "cable bar lateral pulldown",
+                                            target: "lats",
+                                            localUrl: "272.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 3,
+                                        },
+                                        {
+                                            bodyPart: "upper arms",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0031.gif",
+                                            id: "0031",
+                                            name: "barbell curl",
+                                            target: "biceps",
+                                            localUrl: "106.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 3,
+                                        },
+                                    ],
+                                },
+                                {
+                                    tempID: "2023-01-19T23:49:32.364Z",
+                                    workoutTimeArr: [],
+                                    name: "Strong 5X5 - Workout A",
+                                    exerList: [
+                                        {
+                                            bodyPart: "upper legs",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0043.gif",
+                                            id: "0043",
+                                            name: "barbell full squat",
+                                            target: "glutes",
+                                            localUrl: "120.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 5,
+                                        },
+                                        {
+                                            bodyPart: "chest",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0025.gif",
+                                            id: "0025",
+                                            name: "barbell bench press",
+                                            target: "pectorals",
+                                            localUrl: "98.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 5,
+                                        },
+                                        {
+                                            bodyPart: "back",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0027.gif",
+                                            id: "0027",
+                                            name: "barbell bent over row",
+                                            target: "upper back",
+                                            localUrl: "101.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 5,
+                                        },
+                                    ],
+                                },
+                                {
+                                    tempID: "2023-01-19T23:51:37.898Z",
+                                    workoutTimeArr: [],
+                                    name: "Strong 5X5 - Workout B",
+                                    exerList: [
+                                        {
+                                            bodyPart: "upper legs",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0043.gif",
+                                            id: "0043",
+                                            name: "barbell full squat",
+                                            target: "glutes",
+                                            localUrl: "120.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 5,
+                                        },
+                                        {
+                                            bodyPart: "shoulders",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0091.gif",
+                                            id: "0091",
+                                            name: "barbell seated overhead press",
+                                            target: "delts",
+                                            localUrl: "190.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 5,
+                                        },
+                                        {
+                                            bodyPart: "upper legs",
+                                            equipment: "barbell",
+                                            gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0032.gif",
+                                            id: "0032",
+                                            name: "barbell deadlift",
+                                            target: "glutes",
+                                            localUrl: "107.gif",
+                                            metric: "wr",
+                                            timeStamp: [],
+                                            sets: 1,
+                                        },
+                                    ],
+                                },
+                            ],
+                            workoutObj: {},
+                            record: {}
+                        }
+                    },
+                    { new: true },
+                    (err, data) => {
+                        if (err) res.json({ success: false, err: err })
+                        else {
+                            return res.json({ success: true, data: data })
+                        }
+                    }
+                );
             }
-          }
-        );
-      }
-    }
+        }
     )
-  });
+});
 
 router.post("/updateFixTemp", (req, res) => {
     const { userID, fixTempArr } = req.body;
@@ -290,8 +292,8 @@ router.post("/updateWorkoutObj", (req, res) => {
             $set: {
                 workoutObj: workoutObj,
                 record: record,
-                ...(user==='true' && {templateArr: updatedTempArr}),
-                ...(user!=='true' && {fixTempArr: updatedTempArr})
+                ...(user === 'true' && { templateArr: updatedTempArr }),
+                ...(user !== 'true' && { fixTempArr: updatedTempArr })
             }
         },
         { new: true },
